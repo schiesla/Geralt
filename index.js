1100c62a055618e064b3687817b686c2d7a21074
Logan Schiessle <schizzla27@gmail.com>
2023-12-11 20:33:24 -0500

    added index

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import { PORT } from './config.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import router from './routes/user.js';
+dotenv.config();
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
+const app = express();
+app.use(express.json());
+app.use('/api', router);
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+  });
+
+// app.get("/status", (request, response) => {
+//     const status = {
+//         "Status": "Running"
+//      };
+     
+//      response.send(status);
+// });
