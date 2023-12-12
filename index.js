5155bc574ae2564670715c1902cd3c85bf70c704
Logan Schiessle <schizzla27@gmail.com>
2023-12-11 20:36:35 -0500

    removed comment

--- index.js
+++ index.js
@@ -26,10 +26,3 @@ app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
   });
 
-// app.get("/status", (request, response) => {
-//     const status = {
-//         "Status": "Running"
-//      };
-     
-//      response.send(status);
-// });
