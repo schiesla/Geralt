774c8b154d081bac11180dfa9180b467c39c536b
Logan Schiessle <schizzla27@gmail.com>
2023-12-20 19:51:50 -0500

    finished organizing user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,3 +25,35 @@ export async function oneById(req, res) {
         res.status(500).json({message: error.message});
     }
 }
+
+/**
+ * Create new User
+ */
+export async function newUser(req, res) {
+    const data = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+/**
+ * Delete User by ID
+ */
+export async function removeOne(req, res) {
+    try {
+        const id = req.params.id;
+        const data = await User.findByIdAndDelete(id);
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,49 +1,19 @@
 import express from 'express';
 import { loginRequired, profile } from '../controllers/authController.js';
-import { all, oneById } from '../controllers/userController.js';
+import { all, newUser, oneById, removeOne } from '../controllers/userController.js';
 import User from '../models/userModel.js';
 
 const userRoute = express.Router();
 
-const route = (routeUrl) => userRoute.route(routeUrl);
+const route = (routeUrl) => userRoute.route(routeUrl ?? '');
 
 userRoute.route('/tasks')
     .post(loginRequired, profile);
 
 
-route("").get(all);
-route(`/:id`).get(oneById);
-
-/**
- * Create new User
- */
-userRoute.post("", async (req, res) => {
-    const data = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    try {
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave)
-    }
-    catch (error) {
-        res.status(400).json({message: error.message})
-    }
-});
-
-/**
- * Delete User by ID
- */
-userRoute.delete(`/:id`, async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await User.findByIdAndDelete(id);
-        res.send(`Document with ${data.name} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-});
+route()             .get(all);
+route(`/:id`)       .get(oneById);
+route()             .post(newUser);
+route(`/:id`)       .delete(removeOne);
 
 export default userRoute;
