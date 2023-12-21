090f155c43e96b55c3ecb6f9cd09beab0731d8e3
Logan Schiessle <schizzla27@gmail.com>
2023-12-20 20:34:37 -0500

    added dockerfile

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,15 +5,15 @@ import User from '../models/userModel.js';
 
 const userRoute = express.Router();
 
-const route = (routeUrl) => userRoute.route(routeUrl ?? '');
+const route = (routeUrl) => userRoute.route(routeUrl);
 
 userRoute.route('/tasks')
     .post(loginRequired, profile);
 
 
-route()             .get(all);
+route("")           .get(all);
 route(`/:id`)       .get(oneById);
-route()             .post(newUser);
+route("")           .post(newUser);
 route(`/:id`)       .delete(removeOne);
 
 export default userRoute;
