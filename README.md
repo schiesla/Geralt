2f7d507585548754e9ae98d63e6bc3b9fb8b986b
Logan Schiessle <schizzla27@gmail.com>
2024-01-26 16:50:18 -0500

    updated docker

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Geralt
 - `npm install`
 - `npm start` to run
+
+# Docker 
+The docker container will pull the projects package.js run npm install, and then start the node api
+- Build a new image: `docker build --tag geralt-api:[version] .`
+- Run image within containter: `docker run --publish 3000:3000 geralt-api`
+- Run image within containter (detatched): `docker run -d -p 3000:3000 geralt-api`  
+NOTE: for ports -> [host port]:[container port]
+- To show running containers: `docker ps -all` NOTE: "all" flag lists all containers
+- To restart: `docker restart geralt-api`
+- To stop: `docker stop geralt-api` (you can also supply a container ID found listed in the `ps` command)
+- To remove container: `docker rm geralt-api`
+- To log container: `docker logs geralt-api` add `-f` to follow them live
---
+++ server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import { PORT } from './config.js';
+import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+import userRoute from './routes/userRoute.js';
+import authRoute from './routes/authRoute.js';
+import bodyParser from 'body-parser';
+import { authorizeJWT } from './middleware/authMiddleware.js';
+
+// dotenv.config();
+
+const mongoString = process.env.DATABASE_URL;
+const option = {
+  socketTimeoutMS: 30000
+};
+
+mongoose.connect(mongoString, option);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error)
+});
+
+database.once('connected', () => {
+  console.log('Database Connected');
+});
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/auth', authRoute);
+app.use(authorizeJWT);
+app.use('/user', userRoute);
+
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' });
+});
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
